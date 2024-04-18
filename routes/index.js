e13094210996214d1753142e36b9c2ac256ecf4f
Akbar Rahmat Mulyatama <88664674+akbarrahmatm@users.noreply.github.com>
2024-04-18 22:23:06 +0700

    Create index routes, app.js, server.js, errorController, & utils Api Error

---
+++ routes/index.js
@@ -0,0 +1,13 @@
+const router = require("express").Router();
+
+/*
+    Import router disini, Contoh : 
+    const authRouter = require("./authRouter");
+*/
+
+/*
+    Definisikan router disini, Contoh :
+    router.use("/api/v1/auth", authRouter);
+*/
+
+module.exports = router;
