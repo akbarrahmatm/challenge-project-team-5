25766d26fe5e5dc66a6f1c07671591d61ae8e195
petranih <gokilfitra528@gmail.com>
2024-04-19 15:51:11 +0700

    create route customer, and controller customer

---
+++ routes/customerRouter.js
@@ -0,0 +1,16 @@
+const router = require("express").Router();
+
+const customer = require("../controllers/customerController");
+
+// route untuk menambah data
+router.post("/create", customer.createCustomer);
+// route untuk mengambil semua data
+router.get("/get", customer.findCustomers);
+// route untuk mengambil data sesuai id
+router.get("/get/:id", customer.findCustomerById);
+// route untuk mengedit data sesuai id
+router.patch("/update/:id", customer.updateCustomer);
+// route untuk menghapus data sesuai dengan id
+router.delete("/delete/:id", customer.deleteCustomer);
+
+module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,12 @@ const router = require("express").Router();
     Import router disini, Contoh : 
     const authRouter = require("./authRouter");
 */
+const Customer = require("./customerRouter");
 
 /*
     Definisikan router disini, Contoh :
     router.use("/api/v1/auth", authRouter);
 */
+router.use("/api/v1/customer", Customer);
 
 module.exports = router;
