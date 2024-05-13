38f88cf9a189b1cac4824602f1a7cdbd884ea982
Azka Zaki <azkazaki11@gmail.com>
2024-05-13 22:08:06 +0700

    First commit

--- routes/tourPackageRoutes.js
+++ routes/tourPackageRoutes.js
@@ -1,14 +1,13 @@
 const router = require("express").Router();
 
 const tourPackage = require("../controllers/tourPackageController");
-const authenticate = require("../middlewares/authenticate");
 const upload = require('../utils/multerConfig')
 
 router.get("/getAll", tourPackage.findTourPackages)
 router.get("/:id", tourPackage.findTourPackageById)
-router.post("/create", authenticate, upload.single("image"), tourPackage.createTourPackage);
-router.patch("/update/:id", authenticate, tourPackage.updateTourPackage);
-router.delete("/delete/tour/:id", authenticate, tourPackage.deleteTourPackage);
+router.post("/create", upload.single("image"), tourPackage.createTourPackage);
+router.patch("/update/:id", tourPackage.updateTourPackage);
+router.delete("/delete/tour/:id", tourPackage.deleteTourPackage);
 router.post("/gambar", upload.single("image"), tourPackage.createTourPackage);
 
 
